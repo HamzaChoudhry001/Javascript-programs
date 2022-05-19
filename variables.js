0a55b0cb0382d477c7be70049365d31e4fc5d44a
HamzaChoudhry001 <hamzapervez82@gmail.com>
2022-05-19 05:02:45 -0700

    Variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+/*Using var
+Using let
+Using const*/
+var x=10;
+var y=90;
+var c;
+c=x+y
+//let
+let a = 2;
+let b = 3;
+let s = a*b;
+
+//const
+const m = 100;
+const n =200;
+let sum =m+n;
+
+console.log(sum);
