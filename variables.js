ed42dd81ee633927967b6bcc1c0c17d4df8c2e7e
HamzaChoudhry001 <hamzapervez82@gmail.com>
2022-05-19 21:25:42 -0700

    Variables

--- variables.js
+++ variables.js
@@ -15,4 +15,19 @@ const m = 100;
 const n =200;
 let sum =m+n;
 
-console.log(sum);
+//Any datatype
+const pi = 3.14;
+let perso = "John Doe";
+let answer = 'Yes I am!';
+
+
+// You cannot re-declare a variable declared with let or const. but you can with var
+
+// This will not work:
+
+// let carName = "Volvo";
+// let carName;
+
+
+let i = 2 + 3 + "3";
+console.log(i);
